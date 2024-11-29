897ffe355a7a9df3369262e872afdf29e9c25dc6
LadyDi3103 <chinamy2004@gmail.com>
2024-11-29 11:28:33 -0500

    chore(config): update Jest configuration to include test files from src/components and src/shared

--- directo-a-mi-casa/jest.config.js
+++ directo-a-mi-casa/jest.config.js
@@ -12,4 +12,16 @@ export default {
   },
 
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",   // Collect coverage for test files in src/components
+    "src/**/*.test.{ts,tsx}",    // Exclude test files themselves from coverage
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov"],
+  
+  testMatch: [
+    "**/__tests__/**/*.(test|spec).{ts,tsx}",
+  ],
 };
